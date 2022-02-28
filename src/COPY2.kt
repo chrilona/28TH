c98a063cecef2dd7b7c57f115c5fade1077b382a
chrilona <christabelalona@gmail.com>
2022-02-28 15:58:52 +0300

    strings on kotlin

---
+++ src/COPY2.kt
@@ -0,0 +1,23 @@
+
+    fun main(){
+        var y = "African Visionary Fund"
+        var z = "Today"
+        var sentence ="$z we are having guests from $y"
+        println(sentence)
+        //String interpolation
+        var word = "Cremation"
+        println(word[0])
+        println(word[0])
+        //String indexing
+        var mess ="Wednesdays are most loved" + " " +  "I see my crush and we don't talk"
+        println(mess.replace("most","not"))
+        //String replace()
+        val events ="Weddings,Birthdays,Graduations,Initiations"
+        val ceremonies = events.split(",")
+        println(events)
+        println(ceremonies[2])
+        //String split()
+        val day = "MACBOOK MONDAY"
+        println(day.startsWith("M"))
+        println(day.endsWith("Q"))
+    }
